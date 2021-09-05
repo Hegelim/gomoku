ef5ab01e33ce1c730f85cb5dcdda2578207ce2af
Yewen Zhou <yewenzhou@berkeley.edu>
2021-09-05 12:13:43 -0400

    Add board page and index page

--- gomokuproject/gomokuproject/urls.py
+++ gomokuproject/gomokuproject/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from game import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('game/', include('game.urls')),
     path('admin/', admin.site.urls),
 ]
