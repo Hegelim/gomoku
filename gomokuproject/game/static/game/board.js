1028060b7139638360ec78065d3ce6e2a5869f1b
Yewen Zhou <yewenzhou@berkeley.edu>
2021-09-06 14:54:56 -0400

    Add coordinate ids

--- gomokuproject/game/static/game/board.js
+++ gomokuproject/game/static/game/board.js
@@ -1,15 +1,69 @@
 var button = document.querySelectorAll('button');
-var buttonLength = button.length;
+const buttonLength = button.length; // 19 * 19 = 361
 var counter = -1;
+const ROW_LENGTH = 19;
 
 // add click event to all buttons
 for (var i = 0; i < buttonLength; i++) {
-    button[i].addEventListener('click', display);
+    // assign coordinates to all buttons
+    button[i].id = assignId(i+1);
+    button[i].addEventListener('click', play);
+}
+
+// return current row index
+// starting at index 0
+function getRowIndex(i) {
+    var rowIndex = 1;
+    while (i > ROW_LENGTH) {
+        i -= ROW_LENGTH;
+        rowIndex += 1;
+    }
+    return rowIndex;
+}
+
+// get column index
+function getColIndex(i) {
+    if (i % ROW_LENGTH === 0) {
+        return ROW_LENGTH;
+    } else {
+        return i % ROW_LENGTH;
+    }
+}
+
+
+function assignId(i) {
+    var r = getRowIndex(i);
+    var c = getColIndex(i);
+    return "(" + r + "," + c + ")";
+}
+
+
+function clearBoard() {
+    return 0;
+}
+
+// function to check for win
+function ifWin() {
+    return ifWinHorizontal() || ifWinVertical() || ifWinDiagonal();
+}
+
+function ifWinHorizontal() {
+    return 0;
+}
+
+function ifWinVertical() {
+    return 0;
+}
+
+function ifWinDiagonal() {
+    return 0;
 }
 
 
 // click and display
-function display() {
+function play() {
+    alert(this.id);
+
     if (counter === -1) {
         // white goes first
         document.querySelector('h2').innerHTML = "Turn for white!";
