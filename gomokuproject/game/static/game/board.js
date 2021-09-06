2dfccbdc7ec59f61cf67fc54c73a92d90990af4b
Yewen Zhou <yewenzhou@berkeley.edu>
2021-09-06 16:31:46 -0400

    Finish the game! Now able to play, though pretty slow

--- gomokuproject/game/static/game/board.js
+++ gomokuproject/game/static/game/board.js
@@ -1,13 +1,13 @@
-var button = document.querySelectorAll('button');
-const buttonLength = button.length; // 19 * 19 = 361
+var buttons = document.querySelectorAll('button');
+const buttonLength = buttons.length; // 19 * 19 = 361
 var counter = -1;
 const ROW_LENGTH = 19;
 
 // add click event to all buttons
 for (var i = 0; i < buttonLength; i++) {
     // assign coordinates to all buttons
-    button[i].id = assignId(i+1);
-    button[i].addEventListener('click', play);
+    buttons[i].id = assignId(i+1);
+    buttons[i].addEventListener('click', play);
 }
 
 // return current row index
@@ -37,7 +37,6 @@ function assignId(i) {
     return "(" + r + "," + c + ")";
 }
 
-
 function clearBoard() {
     return 0;
 }
@@ -48,31 +47,122 @@ function ifWin() {
 }
 
 function ifWinHorizontal() {
-    return 0;
+    for (var i = 0; i < ROW_LENGTH; i++) {
+        for (var j = 0; j < ROW_LENGTH; j++) {
+            if (sameColor(
+                getButton(i,j),
+                getButton(i,j+1),
+                getButton(i, j+2),
+                getButton(i, j+3),
+                getButton(i, j+4))) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 function ifWinVertical() {
-    return 0;
+    for (var i = 0; i < ROW_LENGTH; i++) {
+        for (var j = 0; j < ROW_LENGTH; j++) {
+            if (sameColor(getButton(i,j), getButton(i+1,j),
+                getButton(i+2, j), getButton(i+3, j), getButton(i+4, j))) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 function ifWinDiagonal() {
-    return 0;
+    for (var i = 0; i < ROW_LENGTH; i++) {
+        for (var j = 0; j < ROW_LENGTH; j++) {
+            if (sameColor(
+                getButton(i,j),
+                getButton(i+1,j+1),
+                getButton(i+2, j+2),
+                getButton(i+3, j+3),
+                getButton(i+4, j+4))) {
+                return true;
+            } else if (sameColor(
+                getButton(i,j),
+                getButton(i-1,j+1),
+                getButton(i-2, j+2),
+                getButton(i-3, j+3),
+                getButton(i-4, j+4))) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+function getButton(r, c) {
+    for (var i = 0; i < buttonLength; i++) {
+        if (getRow(buttons[i]) === r
+            && getCol(buttons[i]) === c) {
+            return buttons[i];
+        }
+    }
+}
+
+function getRow(button) {
+    var leftP = button.id.indexOf('(');
+    var comma = button.id.indexOf(',');
+    return parseInt(button.id.substring(leftP+1, comma));
+}
+
+function getCol(button) {
+    var comma = button.id.indexOf(',');
+    var rightP = button.id.indexOf(')');
+    return parseInt(button.id.substring(comma+1, rightP));
+}
+
+
+function sameColor(s1, s2, s3, s4, s5) {
+    if (s1 === undefined
+        || s2 === undefined
+        || s3 === undefined
+        || s4 === undefined
+        || s5 === undefined) {
+        return false;
+    } else {
+        return (s1.style.background === 'white' &&
+                s2.style.background === 'white' &&
+                s3.style.background === 'white' &&
+                s4.style.background === 'white' &&
+                s5.style.background === 'white') ||
+            (s1.style.background === 'black' &&
+                s2.style.background === 'black' &&
+                s3.style.background === 'black' &&
+                s4.style.background === 'black' &&
+                s5.style.background === 'black');
+    }
 }
 
 
 // click and display
 function play() {
-    alert(this.id);
-
     if (counter === -1) {
         // white goes first
+        document.querySelector('h2').innerHTML = "Turn for black!";
+        this.style.background = 'white';
+        this.style.border = "1px solid grey";
+        // alert(this.id);
+        if (ifWin()) {
+            document.querySelector('h2').innerHTML = "White wins!";
+            alert("White Wins!");
+        }
+    } else if (counter === 1) {
         document.querySelector('h2').innerHTML = "Turn for white!";
         this.style.background = 'black';
         this.style.border = "1px solid grey";
-    } else if (counter === 1) {
-        document.querySelector('h2').innerHTML = "Turn for black!";
-        this.style.background = 'white';
-        this.style.border = "1px solid grey"
+        // alert(this.id);
+        if (ifWin()) {
+            document.querySelector('h2').innerHTML = "Black wins!";
+            alert("Black Wins!");
+        }
     }
     counter *= -1;
 }
